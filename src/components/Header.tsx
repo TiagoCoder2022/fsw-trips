6acc28101cbf8b21bc80cf7e25d9ebced046f7b6
TiagoCoder2022 <tiagonewalves@hotmail.com>
2023-07-13 13:22:18 -0300

    feat: add route to get user reservations

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,25 +1,27 @@
-'use client'
+"use client";
 
-import { signIn, signOut, useSession } from "next-auth/react"
-import Image from "next/image"
-import React from 'react'
-import { AiOutlineMenu } from 'react-icons/ai'
+import React from "react";
+import Image from "next/image";
+import { signIn, signOut, useSession } from "next-auth/react";
+import { AiOutlineMenu } from "react-icons/ai";
 import Link from "next/link";
 
-
-
 const Header = () => {
-  const [menuIsOpen, setMenuIsOpen] = React.useState(false)
-  const {status, data} = useSession()
-  const handleLoginClick = () => signIn()
+  const [menuIsOpen, setMenuIsOpen] = React.useState(false);
+
+  const { status, data } = useSession();
+
+  const handleLoginClick = () => signIn();
+
   const handleLogoutClick = () => {
-    setMenuIsOpen(false)
-    signOut()
-  }
-  const handleMenuClick = () => setMenuIsOpen(!menuIsOpen)
+    setMenuIsOpen(false);
+    signOut();
+  };
+
+  const handleMenuClick = () => setMenuIsOpen(!menuIsOpen);
 
   return (
-   <div className="container mx-auto p-5 py-0 h-[93px] flex justify-between items-center">
+    <div className="container mx-auto p-5 py-0 h-[93px] flex justify-between items-center lg:border-b lg:border-grayLighter">
       <Link href="/">
         <div className="relative h-[32px] w-[182px]">
           <Image src="/logo.png" alt="Full Stack Week" fill />
@@ -27,17 +29,24 @@ const Header = () => {
       </Link>
 
       {status === "unauthenticated" && (
-        <button className="text-primary text-sm font-semibold" onClick={handleLoginClick}>Login</button>      
+        <button className="text-primary text-sm font-semibold" onClick={handleLoginClick}>
+          Login
+        </button>
       )}
+
       {status === "authenticated" && data.user && (
         <div className="flex items-center gap-3 border-grayLighter border border-solid rounded-full p-2 px-3 relative">
-          <AiOutlineMenu size={16} onClick={handleMenuClick}  className="cursor-pointer"/>
+          <AiOutlineMenu size={16} onClick={handleMenuClick} className="cursor-pointer" />
 
-          <Image src={data.user.image!} height={30} width={30} alt={data.user.name!} className="rounded-full shadow-md"/>
+          <Image height={35} width={35} src={data.user.image!} alt={data.user.name!} className="rounded-full shadow-md" />
 
-           {menuIsOpen && (
-            <div className="z-50 absolute top-14 left-0 w-full h-full bg-white rounded-lg shadow-md flex flex-col justify-center items-center">
-              <button className="text-primary text-sm font-semibold" onClick={handleLogoutClick}>
+          {menuIsOpen && (
+            <div className="z-50 absolute top-14 left-0 w-full h-[100px] bg-white rounded-lg shadow-md flex flex-col justify-center items-center">
+              <Link href="/my-trips" onClick={() => setMenuIsOpen(false)}>
+                <button className="text-primary pb-2 border-b border-grayLighter border-solid text-sm font-semibold">Minhas Viagens</button>
+              </Link>
+
+              <button className="text-primary pt-2 text-sm font-semibold" onClick={handleLogoutClick}>
                 Logout
               </button>
             </div>
@@ -45,7 +54,7 @@ const Header = () => {
         </div>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
